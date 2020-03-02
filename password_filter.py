ec78509a28dccb1fa7e993e518d342bd989e6224
@nep_doc <nepzraaz@gmail.com>
2020-03-02 10:52:23 -0600

    Update password_filter.py

--- password_filter.py
+++ password_filter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 __description__ = 'Password Filter'
 __author__ = 'Raj Nepali (@nep_d0c)'
@@ -10,99 +10,96 @@ import re
 import sys
 import time
 
-#list of special chars, I went this route since I was having trouble matching regex
-special_chars = ["!","@","#","$","%","^","&","*","(",")","/","?",".",","]
 
+special_chars = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","+","=","[","{","]","}","|",":",";","'","<",",",">",".","/","?"]
 
 def check_password_complexity(password):
+	'''This looks for the password that matches complexity of any three conditions mentioned below:
+
+		Uppercase
+		Lowercase
+		Numbers
+		Special Characters
+
+	'''
 	#does the password contain uppercase letter
 	upper = re.findall(r'[A-Z]',password)
-        if upper:
-        	hasupper=1
-        else:
-        	hasupper=0
-	#does the password contain lowercase letter
-        lower = re.findall(r'[a-z]',password)
-        if lower:
-        	haslower=1
-        else:
-        	haslower=0
-	#does the password contain digit
-        numbers=re.findall(r'[0-9]',password)
-        if numbers:
-        	hasnumbers=1
-      	else:
-        	hasnumbers=0
-        #this regular expression only matches if it has letters and numbers,otherwise it has special characters, tested on regex101
-        #if re.match("^[a-zA-Z0-9_]*$",password):
-        #       special = 0
-        #else:
-        #       special = 1
-        #But i decided to use list, i was getting false positives, maybe I can find better regex
-
-        if any(spec in password for spec in special_chars):
-        	special = 1
-        else:
-        	special = 0
-        sum = hasupper+haslower+hasnumbers+special
-
-	#if the password matches any three criteria
-	if (sum >=3):
-		return 1
+	if upper:
+		hasupper = 1
 	else:
-		return 0
+		hasupper = 0
+	
+	#does the password contain lowercase letter
+	lower = re.findall(r'[a-z]',password)
 
+	if lower:
+        	haslower = 1
+	else:
+		haslower = 0
+	
+	#does the password contain digit
+	numbers = re.findall(r'[0-9]',password)
+	if numbers:
+		hasnumbers = 1
+	else:
+		hasnumbers = 0
+
+	#does the password contain special characters
+	if any(spec in password for spec in special_chars):
+		special = 1
+	else:
+		special = 0
+
+	#If any of the three conditions is met
+	sum = hasupper + haslower + hasnumbers + special
+
+	if (sum >= 3):
+		return True
 
 def main():
-	count=0
+	count = 0
+	start = time.time()
 
-	if len(sys.argv) != 3:
-		print "[+] Usage: python password_filter.py input_file output_file"
+	if len(sys.argv) < 3:
+		print("[+] Usage: python password_filter.py input_file output_file")
 		exit(1)
+	
 
 	input_file = sys.argv[1]
 	out_file = sys.argv[2]
 
-	start = time.time()
+	
 	with open(input_file) as f:
 		for password in f:
-			password.strip() #You need to strip the password before calculating the length
-			if(len(password) < 9):
-				#For some reason, It is reading extra bit, will test this later
-				print "[!] Password:",password,
-				print " has Less than 8 chars, skipping"
+			password=password.strip() #You need to strip the password before calculating the length
+			
+			if(len(password) < 8):
+				print("[!] Password:", password ," has Less than 8 chars, skipping.")
 				continue
 
-			#if password contains more than two consecutive letters, ignore and move on
-			# Lets use regular expression to test for three consecutive letters
-			substr = re.search(r'([a-z\d])\1\1',password) #not optimal, but works :)
-			#print substr.group(0)
-                	if (substr):
-				print password, " contains Consecutive characters, skipping"
-				continue
 			try:
-				print "[+] Working on password:", password,
+				print('[+] Password:', password, end=' ')
 				#lets check for criteria, any three criteria
 
 				result = check_password_complexity(password)
-      				if (result ==1 ):
-					print "--> looks Good, adding to the file"
+				if (result == True ):
+					
+					print('--> looks Good, adding to the file')
 					count+=1
-              				try:
+					try:
 						fw =open(out_file,'a')
-						fw.write(password)
+						fw.write(password + '\n')
 						fw.close()
 					except Exception as exc:
-						print str(exc)
+						print(str(exc))
 				else:
-					print password, " does not meet complexity requirement, skipping"
-    			except Exception as e:
-				print "Error: %s"%str(e)
+					print(' does not meet complexity requirement, skipping')
+			except Exception as e:
+				print("Error: %s"%str(e))
 
 	end=time.time()
-	print "\n[+] Total run time: ",(end-start)
-	print "\n[+] Total good passwords found and added to the list: ",count
+	print("\n[+] Total run time: ",(end-start))
+	print("\n[+] Total good passwords found and added to the list: ",count)
 
 if __name__ == '__main__':
 	main()
-
